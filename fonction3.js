b1ceb15f7a0a835bdce18c37ac517b677aea6480
melvilrenon <melvilrnn37@gmail.com>
2021-03-26 12:04:23 +0100

    first commit

---
+++ fonction3.js
@@ -0,0 +1,8 @@
+
+var heures = prompt("Dites moi une heure");
+var minutes = prompt("minutes");
+var secondes = prompt("secondes");
+
+var enSecondes = (heures*3600)+(minutes*60)+(secondes);
+
+console.log(enSecondes);
